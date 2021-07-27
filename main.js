8240bc49d69ef344cdf8d92188af218748773b89
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-07-27 15:45:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+noseX = 0;
+noseY = 0;
+
 function preload()
 {
-
+  clown_nose = loadImage("https://i.postimg.cc/DZsv922p/clown.png");
 }
 
 function setup()
@@ -26,6 +29,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
@@ -34,6 +39,7 @@ function gotPoses(results)
 function draw()
 {
   image(video, 0, 0, 300, 300);
+  image(clown_nose , noseX , noseY , 30 , 30);
 }
 
 function take_snapshot()
